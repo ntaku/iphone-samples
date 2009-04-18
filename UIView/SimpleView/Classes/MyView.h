210cfed0b13b092ea0a38cf5c5ea62146e44b868
Takuto Nishioka <ntakuto@gmail.com>
2009-04-18 12:17:46 +0900

    add UIView/SimpleView

---
+++ UIView/SimpleView/Classes/MyView.h
@@ -0,0 +1,16 @@
+//
+//  MyView.h
+//  SimpleView
+//
+//  Created by ntaku on 09/04/18.
+//  Copyright 2009 http://d.hatena.ne.jp/ntaku/. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyView : UIView {
+
+}
+
+@end
---
+++ UIView/SimpleView/Classes/SimpleViewAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  SimpleViewAppDelegate.h
+//  SimpleView
+//
+//  Created by ntaku on 09/04/18.
+//  Copyright http://d.hatena.ne.jp/ntaku/ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleViewAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
